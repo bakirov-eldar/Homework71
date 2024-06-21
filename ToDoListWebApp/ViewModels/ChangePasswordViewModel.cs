74b53117972b66079a8bb8989bbaee8de29e8f0c
Eldar Bakirov <e.bakirov@bakirovlab.ru>
2024-06-21 18:58:56 +0600

    Add ChangePassword localization

--- ToDoListWebApp/ViewModels/ChangePasswordViewModel.cs
+++ ToDoListWebApp/ViewModels/ChangePasswordViewModel.cs
@@ -4,19 +4,18 @@ namespace ToDoListWebApp.ViewModels;
 
 public class ChangePasswordViewModel
 {
-    [Required]
+    [Required(ErrorMessage = "OldPasswordRequired")]
     [DataType(DataType.Password)]
-    [Display(Name = "Текущий пароль")]
+    [Display(Name = "OldPassword")]
     public string OldPassword { get; set; }
 
-    [Required]
-    [StringLength(100, ErrorMessage = "Пароль должен быть не менее {2} и не более {1} символов.", MinimumLength = 6)]
+    [Required(ErrorMessage = "NewPasswordRequired")]
     [DataType(DataType.Password)]
-    [Display(Name = "Новый пароль")]
+    [Display(Name = "NewPassword")]
     public string NewPassword { get; set; }
 
     [DataType(DataType.Password)]
-    [Display(Name = "Подтверждение нового пароля")]
-    [Compare("NewPassword", ErrorMessage = "Пароль и его подтверждение не совпадают.")]
+    [Display(Name = "ConfirmPassword")]
+    [Compare("NewPassword", ErrorMessage = "RequireCompare")]
     public string ConfirmPassword { get; set; }
 }
